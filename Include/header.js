d6bba4232d9c5e8f495e918135c59ffd725c00f1
truemaxdh <truemaxdh@gmail.com>
2020-02-12 01:12:56 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,6 +1,6 @@
 var header = "<div id='header'>" +
              "  <font color='#73AD21'>" +
-             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' onclick="showStaticCont();">▤</a>truemaxdh</b>" +
+             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' onclick='showStaticCont();'>▤</a>truemaxdh</b>" +
              "    <a href="https://github.com/truemaxdh">GitHub</a>" +
              "    <hr>" +
              "  </font>" +
