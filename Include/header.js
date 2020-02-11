952add07320df037670c2750af3f1bc1083b362c
truemaxdh <truemaxdh@gmail.com>
2020-02-12 01:26:32 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,9 +1,25 @@
-var header = "<div id='header'>" +
+var header;
+if (!cookieBread) { 
+  header = "<div id='header'>" +
              "  <font color='#73AD21'>" +
              "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' onclick='showStaticCont();'>▤</a>truemaxdh</b>" +
+             "    <a href='#'>AboutMe</a>" +
+             "    <a href='https://pgmaru.tistory.com'>Tistory</a>" +
+             "    <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
              "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
              "    <hr>" +
              "  </font>" +
              "</div>";
-             
+} else {
+  header = "<div id='header'>" +
+             "  <font color='#73AD21'>" +
+             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' href='https://truemaxdh.github.io'>▤</a>truemaxdh</b>" +
+             "    <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread +
+             "    <a href='https://pgmaru.tistory.com'>Tistory</a>" +
+             "    <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
+             "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
+             "    <hr>" +
+             "  </font>" +
+             "</div>";
+}
 document.write(header);
