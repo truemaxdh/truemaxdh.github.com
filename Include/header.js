e11944cb99a3a3b518903b5620c4dbb7ac154772
truemaxdh <truemaxdh@gmail.com>
2020-02-13 16:06:59 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,20 +1,28 @@
 var header;
 if (typeof cookieBread==="undefined") { 
   header = "<div id='header'>" +
-           "  <a onclick='toggleStaticCont();'>▤</a> <span id='title'>Coding Sketchbook</span>&nbsp;&nbsp;&nbsp;" +
-           "  <a href='#'>About</a> | " +
-           "  <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
-           "  <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
-           "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  <div id='title>" +
+           "    <a onclick='toggleStaticCont();'>▤</a> Coding Sketchbook" +
+           "  </div>" +
+           "  <div id='menu'>" +
+           "    <a href='#'>About</a> | " +
+           "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+           "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
+           "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  </div>" +
            "  <hr>" +
            "</div>";
 } else {
   header = "<div id='header'>" +
-           "  <a href='https://truemaxdh.github.io'>▤</a> <span id='title'>Coding Sketchbook</span>" +
-           "  <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread + " | " +
-           "  <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
-           "  <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
-           "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  <div id='title>" +
+           "    <a href='https://truemaxdh.github.io'>▤</a> <span id='title'>Coding Sketchbook</span>" +
+           "  </div>" +
+           "  <div id='menu'>" +
+           "    <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread + " | " +
+           "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+           "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
+           "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  </div>" +
            "  <hr>" +
            "</div>";
 }
