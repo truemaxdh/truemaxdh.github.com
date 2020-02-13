54f6e05e5f16969dfd1c99052610309d39c78678
truemaxdh <truemaxdh@gmail.com>
2020-02-13 12:29:12 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,19 +1,19 @@
 var header;
 if (typeof cookieBread==="undefined") { 
   header = "<div id='header'>" +
-           "  <a onclick='toggleStaticCont();'>▤</a> <span id='title'>Coding Sketchbook</span>" +
-           "  <a href='#'>About</a>" +
-           "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
-           "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
+           "  <a onclick='toggleStaticCont();'>▤</a> <span id='title'>Coding Sketchbook</span>&nbsp;&nbsp;&nbsp;" +
+           "  <a href='#'>About</a> | " +
+           "  <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+           "  <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
            "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
            "  <hr>" +
            "</div>";
 } else {
   header = "<div id='header'>" +
            "  <a href='https://truemaxdh.github.io'>▤</a> <span id='title'>Coding Sketchbook</span>" +
-           "  <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread +
-           "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
-           "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
+           "  <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread + " | " +
+           "  <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+           "  <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
            "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
            "  <hr>" +
            "</div>";
