c78ab152ea01c2f1b421ade46dee28bf747a3388
truemaxdh <truemaxdh@gmail.com>
2020-02-13 12:16:23 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,7 +1,7 @@
 var header;
 if (typeof cookieBread==="undefined") { 
   header = "<div id='header'>" +
-           "  <a onclick='toggleStaticCont();'>▤</a>Coding Sketchbook" +
+           "  <a onclick='toggleStaticCont();'>▤</a> <span id='title'>Coding Sketchbook</span>" +
            "  <a href='#'>About</a>" +
            "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
            "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
@@ -10,7 +10,7 @@ if (typeof cookieBread==="undefined") {
            "</div>";
 } else {
   header = "<div id='header'>" +
-           "  <a href='https://truemaxdh.github.io'>▤</a>truemaxdh" +
+           "  <a href='https://truemaxdh.github.io'>▤</a> <span id='title'>Coding Sketchbook</span>" +
            "  <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread +
            "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
            "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
