672e025ed6f446b4cb168f07ceb15b4f6e34541f
truemaxdh <truemaxdh@gmail.com>
2020-02-13 11:50:40 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,25 +1,21 @@
 var header;
 if (typeof cookieBread==="undefined") { 
   header = "<div id='header'>" +
-             "  <font color='#73AD21'>" +
-             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' onclick='showStaticCont();'>▤</a>truemaxdh</b>" +
-             "    <a href='#'>AboutMe</a>" +
-             "    <a href='https://pgmaru.tistory.com'>Tistory</a>" +
-             "    <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
-             "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
-             "    <hr>" +
-             "  </font>" +
-             "</div>";
+           "  <a onclick='toggleStaticCont();'>▤</a>truemaxdh" +
+           "  <a href='#'>AboutMe</a>" +
+           "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
+           "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
+           "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  <hr>" +
+           "</div>";
 } else {
   header = "<div id='header'>" +
-             "  <font color='#73AD21'>" +
-             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' href='https://truemaxdh.github.io'>▤</a>truemaxdh</b>" +
-             "    <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread +
-             "    <a href='https://pgmaru.tistory.com'>Tistory</a>" +
-             "    <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
-             "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
-             "    <hr>" +
-             "  </font>" +
-             "</div>";
+           "  <a href='https://truemaxdh.github.io'>▤</a>truemaxdh" +
+           "  <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread +
+           "  <a href='https://pgmaru.tistory.com'>Tistory</a>" +
+           "  <a href='http://pgmaru.blogspot.com/'>Blog</a>" +
+           "  <a href='https://github.com/truemaxdh'>GitHub</a>" +
+           "  <hr>" +
+           "</div>";
 }
 document.write(header);
