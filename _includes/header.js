54303d0b420056cc4abc81230b5317f30eadf0bb
truemaxdh <truemaxdh@gmail.com>
2020-02-14 11:13:38 +0900

    Delete header.js

--- _includes/header.js
+++
@@ -1,9 +0,0 @@
-var header = "<div id='header'>" +
-             "  <font color='#73AD21'>" +
-             "    <b><a style='text-decoration:none;color:#73AD21;cursor:hand;' onclick="showStaticCont();">▤</a>truemaxdh</b>" +
-             "    <a href="https://github.com/truemaxdh">GitHub</a>" +
-             "    <hr>" +
-             "  </font>" +
-             "</div>";
-             
-document.write(header);
