db2345f0b50faa669fed4c42dffed4cb9ef2ea7f
truemaxdh <truemaxdh@gmail.com>
2021-04-28 17:22:14 +0900

    Update header.js

--- Include/header.js
+++ Include/header.js
@@ -1,32 +1,37 @@
 let header;
 if (typeof cookieBread==="undefined") { 
-  header = "<div id='header'>" +
-           "  <div id='title'>" +
-           "    <a onclick='toggleStaticCont();'>▤</a> Coding Sketchbook" +
-           "  </div>" +
-           "  <div id='menu'>" +
-           "    <a href='/2020/02/29/About.html'>About</a> | " +
-           "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
-           "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
-           "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
-           "  </div>" +
-           //"  <hr>" +
-           "</div>";
+  header = 
+    "<div id='header'>" +
+    "  <div id='title'>" +
+    "    <a onclick='toggleStaticCont();'>▤</a>" + 
+    "    Coding Sketchbook" +
+    "    <a onclick='toggleDark();'>◐<a>" +
+    "  </div>" +
+    "  <div id='menu'>" +
+    "    <a href='/2020/02/29/About.html'>About</a> | " +
+    "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+    "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
+    "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
+    "  </div>" +
+    //"  <hr>" +
+    "</div>";
 } else {
-  header = "<div id='header'>" +
-           "  <div id='title'>" +
-           "    <a href='https://truemaxdh.github.io'>▤</a> <span id='title'>Coding Sketchbook</span>" +
-           "  </div>" +
-           "  <div id='menu'>" +
-           "    <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread + " | " +
-           "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
-           "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
-           "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
-           "  </div>" +
-           //"  <hr>" +
-           "</div>";
+  header = 
+    "<div id='header'>" +
+    "  <div id='title'>" +
+    "    <a href='https://truemaxdh.github.io'>▤</a>" +
+    "    <span id='title'>Coding Sketchbook</span>" +
+    "    <a onclick='toggleDark();'>◐<a>" +
+    "  </div>" +
+    "  <div id='menu'>" +
+    "    <a href='https://truemaxdh.github.io'>Home</a>" + cookieBread + " | " +
+    "    <a href='https://pgmaru.tistory.com'>Tistory</a> | " +
+    "    <a href='http://pgmaru.blogspot.com/'>Blog</a> | " +
+    "    <a href='https://github.com/truemaxdh'>GitHub</a>" +
+    "  </div>" +
+    //"  <hr>" +
+    "</div>";
 }
-//header += "<button onclick='toggleDark();'>dark<button>";
 document.write(header);
 
 let darkMode = false;
